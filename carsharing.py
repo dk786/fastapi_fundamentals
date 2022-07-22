cf1fe303fbe8ef51eef7ebc7e331f479a7395577
Dawood <dk786@hotmail.com>
2022-07-22 21:24:29 +0100

    created post and saved to file

--- carsharing.py
+++ carsharing.py
@@ -1,42 +1,29 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
 
+from schemas import load_db, save_db, Car
+
 app = FastAPI()
 
-db = [
-    {"id": 1, "size": "s", "doors": 3, "fuel": "petrol"},
-    {"id": 2, "size": "s", "doors": 4, "fuel": "electric"},
-    {"id": 3, "size": "s", "doors": 4, "fuel": "diesel"},
-    {"id": 4, "size": "m", "doors": 3, "fuel": "petrol"},
-    {"id": 5, "size": "m", "doors": 4, "fuel": "electric"},
-    {"id": 6, "size": "m", "doors": 4, "fuel": "diesel"},
-    {"id": 7, "size": "l", "doors": 3, "fuel": "petrol"},
-    {"id": 8, "size": "l", "doors": 4, "fuel": "electric"},
-    {"id": 9, "size": "l", "doors": 5, "fuel": "diesel"}
-]
-
-
-@app.get("/")
-def welcome():
-    return {"message": "Welcome to the fast api tutorial"}
+db = load_db()
 
 
 @app.get("/api/cars")
 def cars(size: str | None = None, doors: int | None = None, fuel: str | None = None) -> list:
     result = db
     if size:
-        result = [car for car in result if car['size'] == size]
+        result = [car for car in result if car.size == size]
     if doors:
-        result = [car for car in result if car['doors'] == doors]
+        result = [car for car in result if car.doors == doors]
     if fuel:
-        result = [car for car in result if car['fuel'] == fuel]
+        result = [car for car in result if car.fuel == fuel]
 
     return results(result)
 
 
 @app.get("/api/cars/{id}")
 def car_by_id(id: int = None) -> list:
-    result = [car for car in db if car['id'] == id]
+    result = [car for car in db if car.id == id]
     return results(result)
 
 
@@ -45,6 +32,12 @@ def results(result: list) -> list:
         return result
     else:
         raise HTTPException(status_code=404, detail="No car matching provided parameters")
+
+
+@app.post("/api/cars/")
+def add_car(car: Car):
+    db.append(car)
+    save_db(db)
 
 
 if __name__ == "__main__":
--- schemas.py
+++ schemas.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic import BaseModel
 
 
@@ -8,3 +10,14 @@ class Car(BaseModel):
     doors: int = 4
     transmission: str | None = "automatic"
 
+
+def load_db() -> list[Car]:
+    """Load a list of car objects from a json file"""
+    with open("cars.json") as f:
+        return [Car.parse_obj(obj) for obj in json.load(f)]
+
+
+def save_db(cars: list[Car]):
+    with open("cars.json", "w") as f:
+        # noinspection PyArgumentList
+        json.dump([car.dict() for car in cars], f, indent=4)

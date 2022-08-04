828def5498331244d7de6a3e9f33041ecc7e8640
Dawood <dk786@hotmail.com>
2022-08-04 11:50:47 +0100

    end of ch6

--- carsharing.py
+++ carsharing.py
@@ -1,29 +1,49 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from sqlmodel import create_engine, SQLModel, Session, select
 
-from schemas import load_db, save_db, CarInput, CarOutput
+from schemas import CarInput, CarOutput, TripInput, Car, Trip
 
-app = FastAPI()
-db = load_db()
+app = FastAPI(title="Car Sharing App")
+
+engine = create_engine(
+    "sqlite:///carsharing.db",
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
 
 
 @app.get("/api/cars")
-def cars(size: str | None = None, doors: int | None = None, fuel: str | None = None) -> list:
-    result = db
+def cars(size: str | None = None, doors: int | None = None, fuel: str | None = None,
+         session: Session = Depends(get_session)) -> list:
+    query = select(Car)
     if size:
-        result = [car for car in result if car.size == size]
+        query = query.where(Car.size == size)
     if doors:
-        result = [car for car in result if car.doors == doors]
+        query = query.where(Car.doors == doors)
     if fuel:
-        result = [car for car in result if car.fuel == fuel]
+        query = query.where(Car.fuel == fuel)
 
-    return results(result)
+    return session.exec(query).all()
 
 
-@app.get("/api/cars/{id}")
-def car_by_id(id: int = None) -> list:
-    result = [car for car in db if car.id == id]
-    return results(result, id)
+@app.get("/api/cars/{id}", response_model=CarOutput)
+def change_car(id: int, session: Session = Depends(get_session)) -> Car:
+    car = session.get(Car, id)
+    if car:
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
 
 
 def results(result: list, id: int | None = None) -> list:
@@ -35,37 +55,51 @@ def results(result: list, id: int | None = None) -> list:
 
 # create a car entry using the Car object.
 @app.post("/api/cars/", response_model=CarOutput)
-def add_car(car: CarInput) -> CarOutput:
-    new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel, transmission=car.transmission, id=len(db) + 1)
-    db.append(new_car)
-    save_db(db)
+def add_car(car_input: CarInput, session: Session = Depends(get_session)) -> Car:
+    new_car = Car.from_orm(car_input)
+    session.add(new_car)
+    session.commit()
+    session.refresh(new_car)
     return new_car
 
 
 @app.delete("/api/cars/{id}", status_code=204)
-def remove_car(id: int) -> None:
-    match = [car for car in db if car.id == id]
-    if match:
-        car = match[0]
-        db.remove(car)
-        save_db(db)
+def remove_car(id: int, session: Session = Depends(get_session)) -> None:
+    car = session.get(Car, id)
+    if car:
+        session.delete(car)
+        session.commit()
     else:
         raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
 
 
-@app.put("/api/cars/{id}", response_model=CarOutput)
-def change_car(id: int, new_car: CarInput) -> CarOutput:
-    match = [car for car in db if car.id == id]
-    if match:
-        car = match[0]
-        car.fuel = new_car.fuel
+@app.put("/api/cars/{id}", response_model=Car)
+def change_car(id: int, new_car: CarInput,
+               session: Session = Depends(get_session)) -> Car:
+    car = session.get(Car, id)
+    if car:
         car.size = new_car.size
-        car.doors = new_car.doors
+        car.fuel = new_car.fuel
         car.transmission = new_car.transmission
-        save_db(db)
-        return match[0]
+        car.doors = new_car.doors
+        session.commit()
+        return car
     else:
         raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
+
+
+@app.post("/api/cars/{car_id}/trips", response_model=Trip)
+def add_trip(car_id: int, trip_input: TripInput,
+             session: Session = Depends(get_session)) -> Trip:
+    car = session.get(Car, car_id)
+    if car:
+        new_trip = Trip.from_orm(trip_input, update={'car_id': car_id})
+        car.trips.append(new_trip)
+        session.commit()
+        session.refresh(new_trip)
+        return new_trip
+    else:
+        raise HTTPException(status_code=403, detail=f"No car with id = {car_id}")
 
 
 if __name__ == "__main__":
--- schemas.py
+++ schemas.py
@@ -1,26 +1,45 @@
-import json
-
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
 
-class CarInput(BaseModel):
-    size: str | None = "m"
+class TripInput(SQLModel):
+    start: int
+    end: int
+    description: str
+
+
+class TripOutput(TripInput):
+    id: int
+
+
+class Trip(TripInput, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    car_id: int = Field(foreign_key="car.id")
+    car: "Car" = Relationship(back_populates="trips")
+
+
+class CarInput(SQLModel):
+    size: str
     fuel: str | None = "electric"
-    doors: int = 4
-    transmission: str | None = "automatic"
+    doors: int
+    transmission: str | None = "auto"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "size": "m",
+                "doors": 5,
+                "transmission": "manual",
+                "fuel": "hybrid"
+            }
+        }
+
+
+class Car(CarInput, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    trips: list[Trip] = Relationship(back_populates="car")
 
 
 class CarOutput(CarInput):
     id: int
+    trips: list[TripOutput] = []
 
-
-def load_db() -> list[CarOutput]:
-    """Load a list of car objects from a json file"""
-    with open("cars.json") as f:
-        return [CarOutput.parse_obj(obj) for obj in json.load(f)]
-
-
-def save_db(cars: list[CarOutput]):
-    with open("cars.json", "w") as f:
-        # noinspection PyArgumentList
-        json.dump([car.dict() for car in cars], f, indent=4)

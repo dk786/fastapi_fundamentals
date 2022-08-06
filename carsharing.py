606e7233dfb8906a97f00e8dc110556e73be58fd
Dawood <dk786@hotmail.com>
2022-08-06 18:47:12 +0100

    end of ch7

--- carsharing.py
+++ carsharing.py
@@ -1,105 +1,18 @@
+#!/usr/bin/env python3
 import uvicorn
-from fastapi import FastAPI, HTTPException, Depends
-from sqlmodel import create_engine, SQLModel, Session, select
-
-from schemas import CarInput, CarOutput, TripInput, Car, Trip
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+from routers import cars, web
+from db import engine
 
 app = FastAPI(title="Car Sharing App")
-
-engine = create_engine(
-    "sqlite:///carsharing.db",
-    connect_args={"check_same_thread": False},
-    echo=True
-)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
+app.include_router(cars.router)
+app.include_router(web.router)
 
 
 @app.on_event("startup")
 def on_startup():
     SQLModel.metadata.create_all(engine)
-
-
-@app.get("/api/cars")
-def cars(size: str | None = None, doors: int | None = None, fuel: str | None = None,
-         session: Session = Depends(get_session)) -> list:
-    query = select(Car)
-    if size:
-        query = query.where(Car.size == size)
-    if doors:
-        query = query.where(Car.doors == doors)
-    if fuel:
-        query = query.where(Car.fuel == fuel)
-
-    return session.exec(query).all()
-
-
-@app.get("/api/cars/{id}", response_model=CarOutput)
-def change_car(id: int, session: Session = Depends(get_session)) -> Car:
-    car = session.get(Car, id)
-    if car:
-        return car
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
-
-
-def results(result: list, id: int | None = None) -> list:
-    if len(result) > 0:
-        return result
-    else:
-        raise HTTPException(status_code=404, detail=f"No car found matching provided parameters ({id})")
-
-
-# create a car entry using the Car object.
-@app.post("/api/cars/", response_model=CarOutput)
-def add_car(car_input: CarInput, session: Session = Depends(get_session)) -> Car:
-    new_car = Car.from_orm(car_input)
-    session.add(new_car)
-    session.commit()
-    session.refresh(new_car)
-    return new_car
-
-
-@app.delete("/api/cars/{id}", status_code=204)
-def remove_car(id: int, session: Session = Depends(get_session)) -> None:
-    car = session.get(Car, id)
-    if car:
-        session.delete(car)
-        session.commit()
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
-
-
-@app.put("/api/cars/{id}", response_model=Car)
-def change_car(id: int, new_car: CarInput,
-               session: Session = Depends(get_session)) -> Car:
-    car = session.get(Car, id)
-    if car:
-        car.size = new_car.size
-        car.fuel = new_car.fuel
-        car.transmission = new_car.transmission
-        car.doors = new_car.doors
-        session.commit()
-        return car
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
-
-
-@app.post("/api/cars/{car_id}/trips", response_model=Trip)
-def add_trip(car_id: int, trip_input: TripInput,
-             session: Session = Depends(get_session)) -> Trip:
-    car = session.get(Car, car_id)
-    if car:
-        new_trip = Trip.from_orm(trip_input, update={'car_id': car_id})
-        car.trips.append(new_trip)
-        session.commit()
-        session.refresh(new_trip)
-        return new_trip
-    else:
-        raise HTTPException(status_code=403, detail=f"No car with id = {car_id}")
 
 
 if __name__ == "__main__":
---
+++ routers/cars.py
@@ -0,0 +1,88 @@
+from fastapi import Depends, HTTPException, APIRouter
+from sqlmodel import Session, select
+
+from db import get_session
+from schemas import CarOutput, Car, CarInput, TripInput, Trip
+
+router = APIRouter(prefix="/api/cars")
+
+
+@router.get("/{id}", response_model=CarOutput)
+def change_car(id: int, session: Session = Depends(get_session)) -> Car:
+    car = session.get(Car, id)
+    if car:
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
+
+
+@router.get("/")
+def get_cars(size: str | None = None, doors: int | None = None, fuel: str | None = None,
+             session: Session = Depends(get_session)) -> list:
+    query = select(Car)
+    if size:
+        query = query.where(Car.size == size)
+    if doors:
+        query = query.where(Car.doors == doors)
+    if fuel:
+        query = query.where(Car.fuel == fuel)
+
+    return session.exec(query).all()
+
+
+@router.post("/", response_model=Car)
+def add_car(car_input: CarInput, session: Session = Depends(get_session)) -> Car:
+    new_car = Car.from_orm(car_input)
+    session.add(new_car)
+    session.commit()
+    session.refresh(new_car)
+    return new_car
+
+
+@router.delete("/{id}", status_code=204)
+def remove_car(id: int, session: Session = Depends(get_session)) -> None:
+    car = session.get(Car, id)
+    if car:
+        session.delete(car)
+        session.commit()
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
+
+
+def results(result: list, id: int | None = None) -> list:
+    if len(result) > 0:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail=f"No car found matching provided parameters ({id})")
+
+
+# create a car entry using the Car object.
+
+
+@router.post("/{car_id}/trips", response_model=Trip)
+def add_trip(car_id: int, trip_input: TripInput,
+             session: Session = Depends(get_session)) -> Trip:
+    car = session.get(Car, car_id)
+    if car:
+        new_trip = Trip.from_orm(trip_input, update={'car_id': car_id})
+        car.trips.append(new_trip)
+        session.commit()
+        session.refresh(new_trip)
+        return new_trip
+    else:
+        raise HTTPException(status_code=403, detail=f"No car with id = {car_id}")
+
+
+@router.put("/{id}", response_model=Car)
+def change_car(id: int, new_car: CarInput,
+               session: Session = Depends(get_session)) -> Car:
+    car = session.get(Car, id)
+    if car:
+        car.size = new_car.size
+        car.fuel = new_car.fuel
+        car.transmission = new_car.transmission
+        car.doors = new_car.doors
+        session.commit()
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with Id {id} found.")
